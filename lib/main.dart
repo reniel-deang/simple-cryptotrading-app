0a300988d34f0fb95daaee711866447bbd9e65f4
riveramiko64 <mikorivera178@gmail.com>
2024-04-10 01:03:52 +0800

    added Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,194 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(MaterialApp(
+    home: Homepage(),
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      useMaterial3: true
-    ),
   ));
 }
 
-class Start extends StatefulWidget {
-  const Start({super.key});
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
 
-  @override
-  State<Start> createState() => _StartState();
-}
-
-class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text('Portfolio'),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(50.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.white,
+                blurRadius: 1.0,
+                spreadRadius: 1.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.account_circle_outlined, color: Colors.black, size: 30,),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.qr_code_scanner_outlined, color: Colors.black, size: 30,),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text('Total balance', style: TextStyle(color: Colors.grey),),
+                            Text(
+                              '₱ 7,466.20',
+                              style: TextStyle(color: Colors.black, fontSize: 30),
+                            ),
 
+                            SizedBox(height: 20,),
+                            Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center, // Change to center
+                                children: [
+                                  Container(
+                                    width: 150,
+                                    height: 50,
+                                    margin: EdgeInsets.only(right: 20), // Add margin to the right
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(27, 27, 27, 1),
+                                    ),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.transparent,
+                                        elevation: 0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'Buy',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 150,
+                                    height: 50,
+                                    margin: EdgeInsets.only(left: 20), // Add margin to the left
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromRGBO(27, 27, 27, 1),
+                                    ),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.transparent,
+                                        elevation: 0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'Sell',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+
+
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+
+
+                      Container(
+                        height: 400,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(242, 243, 245, 1),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(20),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text('Assets'),
+                                              IconButton(
+                                                onPressed: () {},
+                                                icon: Icon(Icons.add),
+                                              )
+                                            ],
+                                          ),
+                                          SizedBox(height: 20,),
+                                          Row(
+                                            children: [
+                                              Image.asset('assets/bitcoin.png',height: 40,),
+                                              SizedBox(width: 10,),
+                                              Text('Bitcoin'),
+                                            ],
+                                          ),
+                                          SizedBox(height: 20,),
+                                          Row(
+                                            children: [
+                                              Image.asset('assets/etherium.png',height: 40,),
+                                              SizedBox(width: 10,),
+                                              Text('Ethereum'),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
